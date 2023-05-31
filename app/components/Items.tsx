a3510d3598db25f4dfec6d4cc1d4dd6dcd34336d
Ellipog <45790498+Ellipog@users.noreply.github.com>
2023-05-31 11:11:42 +0200

    Added Products

--- app/components/Items.tsx
+++ app/components/Items.tsx
@@ -7,12 +7,11 @@ type Props = {
 
 const Navbar = (props: Props) => {
 	return (
-		<div className=" p-16 over">
-			<div className="flex flex-col justify-center items-center p-8 bg-[#EEE3CB]">
-				<img src={props.items.image} alt="Datamus" className="mb-4" />
-				<h1>{props.items.name}</h1>
-				<h1>{props.items.price}</h1>
-				<button className="p-2 text-center rounded bg-white w-72"></button>
+		<div className="p-8 over">
+			<div className="flex flex-col justify-center items-center p-8 bg-[#EEE3CB] rounded">
+				<img src={props.items.image} alt={props.items.name} className="mb-4 bg-[#D7C0AE] rounded object-contain p-4 w-full h-60" />
+				<h1 className="text-[#967E76] font-medium">{props.items.name}</h1>
+				<h1 className="text-[#967E76] font-medium">{props.items.price} kr</h1>
 			</div>
 		</div>
 	);
