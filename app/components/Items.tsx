c972a2b24fc8e27f8fbe009918a6b4fe5886ce0c
Ellipog <45790498+Ellipog@users.noreply.github.com>
2023-06-02 09:52:45 +0200

    Added product pages

--- app/components/Items.tsx
+++ app/components/Items.tsx
@@ -1,44 +1,89 @@
+"use client";
+
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { faShoppingCart, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { toast } from "react-hot-toast";
 import { useDispatch } from "react-redux";
 import { addToCart } from "../redux/cart.slice";
+import { useState } from "react";
 
 type Props = {
-  items: any;
+	items: any;
 };
 
 const Navbar = (props: Props) => {
-  const random = Math.floor(Math.random() * 6) - 3;
+	const random = Math.floor(Math.random() * 6) - 3;
+	const [productPage, setProductPage] = useState(false);
 
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  return (
-    <div className="p-8">
-      <div className="fixed z-10 w-full">
-        <img src="https://pngimg.com/uploads/pin/pin_PNG76.png" alt="pin" className="h-10 relative left-[6%]" />
-      </div>
-      <div className="flex flex-col justify-center items-center p-8 bg-[#EEE3CB] shadow-xl transition-all" style={{ rotate: random + "deg" }}>
-        <img src={props.items.image} alt={props.items.name} className="mb-4 bg-[#D7C0AE] object-contain p-4 w-full h-56" />
-        <div className="flex flex-row justify-between items-center w-full">
-          <div className="bg-[#fff3d8] flex justify-center flex-col items-center p-2 w-9/12 shadow">
-            <h1 className="text-[#967E76] font-medium">{props.items.name}</h1>
-            <h1 className="text-[#967E76] font-medium">{props.items.price} kr</h1>
-          </div>
-          <FontAwesomeIcon
-            icon={faShoppingCart}
-            className="w-10 h-10 text-[#967E76] hover: cursor-pointer"
-            onClick={() => {
-              toast.success("Added to cart!");
-              dispatch(addToCart(props.items));
-            }}
-          />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="p-8">
+			<div className="fixed z-10 w-full">
+				<img src="https://pngimg.com/uploads/pin/pin_PNG76.png" alt="pin" className="h-10 relative left-[6%]" />
+			</div>
+			<div className="flex flex-col justify-center items-center p-8 bg-[#EEE3CB] shadow-xl transition-all" style={{ rotate: random + "deg" }}>
+				<img src={props.items.image} alt={props.items.name} className="mb-4 bg-[#D7C0AE] object-contain p-4 w-full h-56" />
+				<div className="flex flex-row justify-between items-center w-full">
+					<div
+						className="bg-[#fff3d8] flex justify-center flex-col items-center p-2 w-9/12 shadow"
+						onClick={() => {
+							setProductPage(true);
+						}}
+					>
+						<h1 className="text-[#967E76] font-medium">{props.items.name}</h1>
+						<h1 className="text-[#967E76] font-medium">{props.items.price} kr</h1>
+					</div>
+					<FontAwesomeIcon
+						icon={faShoppingCart}
+						className="w-10 h-10 text-[#967E76] hover: cursor-pointer"
+						onClick={() => {
+							toast.success("Added to cart!");
+							dispatch(addToCart(props.items));
+						}}
+					/>
+				</div>
+			</div>
+			{productPage && (
+				<div className="fixed h-screen w-screen top-0 left-0 flex justify-center items-center z-20 text-[#967E76]" onClick={() => setProductPage(false)}>
+					<div className="h-3/6 w-6/12 bg-[#e9dec7] shadow-xl flex justify-center items-center gap-14">
+						<div className="w-2/5 h-96">
+							<img src={props.items.image} alt={props.items.name} className="mb-4 bg-[#D7C0AE] object-contain p-4 w-full h-full" />
+						</div>
+						<div className="w-2/5 flex justify-center items-center flex-col h-96 gap-5">
+							<div className="bg-[#fff3d8] flex justify-between flex-col items-center p-5 w-full h-full shadow gap-3">
+								<p className="text-xl">{props.items.name}</p>
+								<p className="text-sm">{props.items.desc}</p>
+								<div className="flex w-full flex-col">
+									<div className="flex w-full flex-row justify-between">
+										<p>Without mva:</p>
+										<p>{props.items.price} kr</p>
+									</div>
+									<div className="flex w-full flex-row justify-between">
+										<p>With mva:</p>
+										<p>{props.items.price + props.items.price * 0.25} kr</p>
+									</div>
+								</div>
+							</div>
+							<button
+								className="p-2 text-center bg-[#fff3d8] text-[#967E76] w-72 shadow hover:bg-[#f5e9ce] hover:rotate-[0.5deg] transition-all"
+								onClick={() => {
+									toast.success("Added to cart!");
+									dispatch(addToCart(props.items));
+								}}
+							>
+								ADD TO CART
+							</button>
+						</div>
+					</div>
+					<div className="w-1/12 h-10 bg-[#D7C0AE] fixed right-[28.4rem] top-[17rem] rotate-[38deg] shadow"></div>
+					<div className="w-1/12 h-10 bg-[#D7C0AE] fixed left-[28.4rem] top-[17rem] rotate-[-38deg] shadow"></div>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -5,26 +5,66 @@ import Items from "./components/Items";
 import { useState } from "react";
 
 export default function Home() {
-  const [items, setItems] = useState([
-    { name: "Wireless Mouse", price: 599, image: "https://i.imgur.com/XRZHd9V.png" },
-    { name: "Computer Monitor", price: 1299, image: "https://i.imgur.com/vjvVwY3.png" },
-    { name: "Keyboard", price: 799, image: "https://i.imgur.com/YdK6tZj.png" },
-    { name: "MS License - 1 month", price: 10999, image: "https://i.imgur.com/tdSu85T.png" },
-    { name: "Duramont Chair", price: 99, image: "https://i.imgur.com/FWIwxLA.png" },
-    { name: "IPhone 2", price: 12999, image: "https://i.imgur.com/scs9tUA.png" },
-    { name: "Red Computer Desk", price: 16999, image: "https://i.imgur.com/3eiCseG.png" },
-    { name: "Computer Camera", price: 299, image: "https://i.imgur.com/KdLhWoU.png" },
-  ]);
+	const [items, setItems] = useState([
+		{
+			name: "Wireless Mouse",
+			price: 599,
+			image: "https://i.imgur.com/XRZHd9V.png",
+			desc: "Are you tired of dragging your mouse around like a caveman? Do you want to experience the thrill of rolling a ball with your thumb? Then you need the ball wheel mouse, the ultimate device for thumb enthusiasts. It's ergonomic, it's stylish, and it's fun. Order yours today and get ready to roll!",
+		},
+		{
+			name: "Computer Monitor",
+			price: 1299,
+			image: "https://i.imgur.com/vjvVwY3.png",
+			desc: "Are you looking for a vintage piece of technology to spice up your home office? Look no further than this old computer monitor! It has a CRT screen that flickers like a candle, a bulky frame that takes up half your desk, and a resolution that makes everything look pixelated. What more could you ask for?",
+		},
+		{
+			name: "Keyboard",
+			price: 799,
+			image: "https://i.imgur.com/YdK6tZj.png",
+			desc: "Looking for a vintage typing experience? Look no further than this old keyboard! It has all the features you love: loud clicks, sticky keys, and a charming layer of dust. Hurry up and get it before it becomes a museum piece!",
+		},
+		{
+			name: "MS License - 1 month",
+			price: 10999,
+			image: "https://i.imgur.com/tdSu85T.png",
+			desc: "Looking for a blast from the past? Try Windows 95 for one month and experience the nostalgia of clunky graphics, slow loading times and frequent crashes. You'll be amazed at how far technology has come since then. Hurry up and get your Windows 95 license today, before it becomes obsolete again!",
+		},
+		{
+			name: "Duramont Chair",
+			price: 99,
+			image: "https://i.imgur.com/FWIwxLA.png",
+			desc: "Looking for a comfortable and stylish chair for your home or office? Look no further than the leather duramont chair! This chair features a sleek design, a padded seat and back, and an adjustable height and tilt mechanism. Whether you need to work, relax, or nap, the leather duramont chair has you covered!",
+		},
+		{
+			name: "IPhone 2",
+			price: 12999,
+			image: "https://i.imgur.com/scs9tUA.png",
+			desc: "Are you tired of touchscreens and apps? Do you long for the good old days of dialing numbers by hand? Then you need the iPhone 2 rotary phone! It's the perfect blend of retro and modern, with a sleek design and a satisfying click. Just plug it into your wall socket and enjoy the nostalgia. Order now and get a free pair of earbuds!",
+		},
+		{
+			name: "Red Computer Desk",
+			price: 16999,
+			image: "https://i.imgur.com/3eiCseG.png",
+			desc: "Are you looking for a very small red computer desk? Look no further than this one! It's so small, you can fit it in your pocket. It's so red, you'll never lose sight of it. It's so computer-friendly, it has a built-in USB port. Don't miss this amazing offer!",
+		},
+		{
+			name: "Computer Camera",
+			price: 299,
+			image: "https://i.imgur.com/KdLhWoU.png",
+			desc: "Looking for a vintage webcam? Look no further than this ancient relic from the early 2000s. It has a whopping 0.3 megapixels resolution, a built-in microphone that sounds like a tin can, and a USB cable that barely fits in your laptop. Hurry up and get it before it becomes a museum piece!",
+		},
+	]);
 
-  return (
-    <main className="flex w-screen h-screen flex-col items-center justify-center bg-[#c6cdd4]">
-      <Navbar />
-      <h1 className="text-[#967E76] font-bold text-4xl mt-8 bg-[#fff3d8] p-3 rotate-1 shadow">PRODUCTS</h1>
-      <div className="h-full w-9/12 grid grid-cols-4">
-        {Object.values(items).map((items, i) => {
-          return <Items key={i} items={items} />;
-        })}
-      </div>
-    </main>
-  );
+	return (
+		<main className="flex w-screen h-screen flex-col items-center justify-center bg-[#c6cdd4]">
+			<Navbar />
+			<h1 className="text-[#967E76] font-bold text-4xl mt-8 bg-[#fff3d8] p-3 rotate-1 shadow">PRODUCTS</h1>
+			<div className="h-full w-9/12 grid grid-cols-4">
+				{Object.values(items).map((items, i) => {
+					return <Items key={i} items={items} />;
+				})}
+			</div>
+		</main>
+	);
 }
